88bbcbe32166cc0bb2cb55898e82fba80f760d98
jess-bk <74265740+jess-bk@users.noreply.github.com>
2022-08-18 13:07:06 +0100

    Fifth Commit Newsletter Component Created Customer Email Offers

---
+++ client/src/components/Newsletter.jsx
@@ -0,0 +1,61 @@
+import { Send } from "@material-ui/icons";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 60vh;
+  background-color: #f9f9f9;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+  margin-bottom: 20px;
+`;
+
+const Description = styled.div`
+  font-size: 24px;
+  font-weight: 300;
+  margin-bottom: 20px;
+`;
+
+const InputContainer = styled.div`
+  width: 50%;
+  height: 40px;
+  background-color: white;
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid lightgray;
+`;
+
+const Input = styled.input`
+  border: none;
+  flex: 8;
+  padding-left: 20px;
+`;
+
+const Button = styled.button`
+  flex: 1;
+  border: none;
+  background-color: lightgrey;
+  color: black;
+`;
+
+const Newsletter = () => {
+  return (
+    <Container>
+      <Title>Newsletter</Title>
+      <Description>Get The Latest Offers And Best Price</Description>
+      <InputContainer>
+        <Input placeholder="Enter your email" />
+        <Button>
+          <Send />
+        </Button>
+      </InputContainer>
+    </Container>
+  );
+};
+
+export default Newsletter;
