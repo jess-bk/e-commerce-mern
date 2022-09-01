c0b8eab6939aca8368300f3f8ecc958435e3ca2e
jess-bk <74265740+jess-bk@users.noreply.github.com>
2022-09-01 17:00:57 +0100

    Server Side Delete Product Functionality Created

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -26,4 +26,13 @@ const updateProducts = async (req, res) => {
   }
 };
 
-module.exports = { createProduct, updateProducts };
+const deleteProduct = async = (req,res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id)
+    res.status(200).json(product)
+  } catch (error){
+    res.status(500).json(error)
+  }
+}
+
+module.exports = { createProduct, updateProducts,deleteProduct };
