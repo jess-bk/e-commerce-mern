2a6bd874fbb8cb16e13d3946ff400a90cc59f3b2
jess-bk <74265740+jess-bk@users.noreply.github.com>
2022-09-03 13:02:18 +0100

    Created Dummy User To Navigate Home When Logged In

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,16 @@ import Product from "./pages/Product";
 import Cart from "./pages/Cart";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
 const App = () => {
+  const user = true;
+
   return (
     <Router>
       <Routes>
@@ -14,7 +21,7 @@ const App = () => {
         <Route path="/products/:category" element={<ProductList />} />
         <Route path="/product/:id" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
